dafea53cb4d477f19562d29414d536cf91cb0eb4
GuenounouAlem <123456789+slayershad05@gmail.com>
2024-10-31 14:08:17 +0100

    some logic done, bug on turn switches

--- script.js
+++ script.js
@@ -1,26 +1,99 @@
-function createPlayer(name, symbol) {
-    return { name, symbol }
+
+// Player Object //
+
+function createPlayer(name, symbol, turn = true) {
+
+    function toggleTurn() {
+        turn = !turn
+    }
+    return { name, symbol, turn, toggleTurn }
 }
 
+
+
+// Game Board Object //
+
 function createBoard() {
-    const boardArray = [null, null, null,
-                        null, null, null,
-                        null, null, null
+    const boardArray = [[null, null, null],
+                        [null, null, null],
+                        [null, null, null]
     ]
-    function drawMark(index, symbol) {
-        if (boardArray[index] !== null) {
+    function drawMark(ix, iy, symbol) {
+        if (boardArray[iy][ix] !== null) {
             console.log('Impossible')
             return
         }
-        boardArray[index] = symbol
+        boardArray[iy][ix] = symbol
     }
-    function printBoard(){
-        console.log(boardArray)
+    function getCase(ix ,iy){
+        return boardArray[iy][ix]
     }
-    function getCase(index){
-        return boardArray[index]
+    function getBoard() {
+        return boardArray
     }
-    return {drawMark, printBoard, getCase}
+    return {drawMark, getBoard, getCase}
+}
+
+// Game Object //
+
+function createGame() {
+
+    let round = 0
+    const maxRound = 9
+    const gameBoard = createBoard()
+    const players = [createPlayer('playerX', 'x'), createPlayer('playerO', 'o', false)]
+
+
+    function checkWinner(player, gameBoard) {
+        const winningCombinations = [
+          // rows
+          [[0, 0], [0, 1], [0, 2]],
+          [[1, 0], [1, 1], [1, 2]],
+          [[2, 0], [2, 1], [2, 2]],
+          // columns
+          [[0, 0], [1, 0], [2, 0]],
+          [[0, 1], [1, 1], [2, 1]],
+          [[0, 2], [1, 2], [2, 2]],
+          // diagonals
+          [[0, 0], [1, 1], [2, 2]],
+          [[0, 2], [1, 1], [2, 0]],
+        ];
+      
+        for (const combination of winningCombinations) {
+          if (combination.every(([x, y]) => gameBoard.getCase(x, y) === player.symbol)) {
+            console.log(player.name + ' WINS');
+            round = maxRound
+            return true;
+          }
+        }
+        return false;
+      }
+
+    function playRound(ix, iy) {
+
+        console.log(gameBoard.getBoard());
+        
+
+        if (round > maxRound) {
+            console.log('game ended')
+            return
+        }
+        if (players[0].turn) {
+            gameBoard.drawMark(ix, iy, players[0].symbol)
+            round++
+            checkWinner(players[0], gameBoard)
+            players[0].toggleTurn()
+            players[1].toggleTurn()
+        } else {
+            gameBoard.drawMark(ix, iy, players[1].symbol)
+            round++
+            checkWinner(players[1], gameBoard)
+            players[1].toggleTurn()
+            players[0].toggleTurn()
+        }
+    }
+
+    return { checkWinner, playRound}
 }
 
 
@@ -50,22 +123,13 @@ function createBoard() {
 
 // ------------------------------------ TesTing ------------------------------- //
 
-const playerX = createPlayer('player x', 'x')
-const playerO = createPlayer('player o', 'o')
+let newGame = createGame()
 
-console.log(playerO, playerX)
-
-const board = createBoard()
-
-console.log(board);
-
-board.printBoard()
-
-board.drawMark(2, playerO.symbol)
-
-board.printBoard()
-
-console.log(board.getCase(1));
+while (true) {
+    let x = prompt('x position ?')
+    let y = prompt('y position ?')
+    newGame.playRound(Number(x), Number(y))
+}
 
 
 
